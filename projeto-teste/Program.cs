bcfb9aa74a72ef3e7e710c710bf2cef259f551cb
Pietro Cozzer <pietrocozzer@hotmail.com>
2019-07-09 10:19:32 -0300

    Acrescentando tracos

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -4,10 +4,12 @@ namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
 
+            Console.WriteLine("----------------------------");
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
             Console.WriteLine("Tchau!");
+            Console.WriteLine("----------------------------");
 
             Console.ReadLine();
         }
